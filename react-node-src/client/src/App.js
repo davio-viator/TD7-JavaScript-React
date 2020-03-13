37837987c228bc9b8a83bacc25b2add46bcd0671
basile-savouret <47100280+basile-savouret@users.noreply.github.com>
2020-03-13 17:22:45 +0100

    client updated

--- react-node-src/client/src/App.js
+++ react-node-src/client/src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {makeStyles} from "@material-ui/core/styles";
+import Title from './Title';
+import Adherent from './Adherent';
+import Available from './BooksAvailable';
+import Borrowed from './BorrowedBooks';
 
-function App() {
+const useStyles = makeStyles(theme => ({
+  background: {
+    backgroundColor: '#212324',
+    height: 'auto',
+    minHeight:'100vh',
+    width:'100%',
+    position:'absolute'
+  },
+  flex: {
+    display:'flex',
+    justifyContent: 'space-around',
+    width:'100%',
+    flexWrap:'wrap',
+    marginTop:'3%'
+  }
+}));
+
+const App = () => {
+  const classes = useStyles();
+  const [listAdherent, setAdherent] = useState([{name:"Roger"},{name:"Jean-Pierre"},{name:"Luc"}]);
+  const [listAvailable, setAvailable] = useState([{name:"moi"}]);
+  const [listBorrowed, setBorrowed] = useState([{name:"toi"}]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.background}>
+      <Title/>
+      <div className={classes.flex}>
+        <Adherent listAdherent={listAdherent}/>
+        <Available listAvailable={listAvailable}/>
+        <Borrowed listBorrowed={listBorrowed}/>
+      </div>
     </div>
   );
 }
